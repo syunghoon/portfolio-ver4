ed0eeb0f2e9a7a1fb2dc7be71a3d1f61f2c1901b
syunghoon <shcho2109@gmail.com>
2025-11-26 20:33:03 +0900

    style: 디자인 스타일링 최소화

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,9 @@ function Home() {
   return (
     <section className="page-section">
       <h1>Product Designer</h1>
+      <div className="content-toolbar">
+        <p className="category-filter__button">테스트 이름</p>
+      </div>
     </section>
   );
 }
